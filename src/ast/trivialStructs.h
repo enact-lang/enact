e1971bfe5d7e4013a86c17a5613092904d4853f0
Dandigit <37099129+Dandigit@users.noreply.github.com>
2019-01-17 09:45:28 +1000

    Add function declarations

--- src/ast/trivialStructs.h
+++ src/ast/trivialStructs.h
@@ -1,13 +1,18 @@
 #ifndef ENACT_GIVENCASE_H
 #define ENACT_GIVENCASE_H
 
-#include "../ast/Stmt.h"
+#include "Stmt.h"
 
 class Stmt;
 
 struct GivenCase {
     std::shared_ptr<Expr> value;
     std::vector<std::shared_ptr<Stmt>> body;
+};
+
+struct Parameter {
+    Token name;
+    std::string typeName;
 };
 
 #endif //ENACT_GIVENCASE_H
